1270a5174876745bf9282dcad7b168bc730ff2d1
aleysoliman <aleykhaledsoliman@gmail.com>
2025-01-27 23:12:21 +0200

    part3 (3.1 - 3.6)

---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const app = express();
+
+let persons = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.use(express.json());
+
+app.get("/api/persons", (req, res) => {
+  res.send(persons);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const person = persons.find((p) => p.id === id);
+  if (person) {
+    return res.send(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  persons = persons.filter((p) => p.id !== id);
+  res.status(204).end();
+});
+
+app.post("/api/persons/", (req, res) => {
+  const body = req.body;
+  if (!body.name || !body.number) {
+    return res.status(400).end();
+  }
+  if (persons.find((p) => p.name === body.name)) {
+    return res.status(409).send({ error: "name must be unique" });
+  }
+  const newPerson = {
+    name: body.name,
+    number: body.number,
+  };
+  newPerson.id = parseInt(Math.random() * 10000).toString();
+  persons = persons.concat(newPerson);
+  res.send(newPerson);
+});
+
+app.get("/info", (req, res) => {
+  const len = persons.length;
+  const now = new Date();
+  const reply = `<p>Phonebook has info for ${len} people</p> <p>${now}</p>`;
+  res.send(reply);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log("App is listening on port 3001 . . .");
+});
